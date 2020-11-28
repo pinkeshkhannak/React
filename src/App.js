8bd8c3c5dc6c774af71901b65b6b56d97da689ee
unknown <pinkeshkhanna56@gmail.com>
2020-11-28 10:51:28 +0530

    my react project

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import NavBar from "./components/navbar";
+import React, { Component } from "react";
+import "./App.css";
+import Counters from "./components/counters";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.length}></NavBar>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          ></Counters>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
